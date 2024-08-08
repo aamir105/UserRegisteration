b0495cecadec13da0ee601e2b1fa659cf5f03b76
aamir105 <aamiranwer2018@gmail.com>
2024-08-08 13:37:52 +0530

    Added:UC1_First_Name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.printf("Hello and welcome!");
-
+            System.out.println("Enter first name");
+            String fName=sc.next();
+            String pattern = "^[A-Z][a-zA-Z]{2,}$";
+            Pattern compiledPattern = Pattern.compile(pattern);
+            Matcher matcher = compiledPattern.matcher(fName);
+            System.out.println(matcher.matches());
     }
 }
