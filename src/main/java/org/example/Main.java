e7dbdff1c6e7d344c0b3e31cbc119c39cc00acb9
aamir105 <aamiranwer2018@gmail.com>
2024-08-08 14:38:11 +0530

    Added:UC3_eMail

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,40 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+            String pattern = "^[A-Z][a-zA-Z]{2,}$";
             Scanner sc = new Scanner(System.in);
 
             System.out.println("Enter first name");
             String fName=sc.next();
-            System.out.println("Enter last name");
-            String lName=sc.next();
-            String pattern = "^[A-Z][a-zA-Z]{2,}$";
             Pattern compiledPattern = Pattern.compile(pattern);
             Matcher matcher = compiledPattern.matcher(fName);
-            System.out.println(matcher.matches());
+            if (matcher.matches()){
+                    System.out.println("Valid");
+            }
+            else {
+                    System.out.println("Invalid");
+            }
+
+            System.out.println("Enter last name");
+            String lName=sc.next();
             Matcher matcher2 = compiledPattern.matcher(lName);
-            System.out.println(matcher2.matches());
+            if (matcher2.matches()){
+                    System.out.println("Valid");
+            }
+            else {
+                    System.out.println("Invalid");
+            }
 
 
-
+            System.out.print("Enter email address: ");
+            String email = sc.nextLine();
+            String pattern2 = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[-a-zA-Z0-9]{0,61}[a-zA-Z0-9])(?:\\.[a-zA-Z]{2,})?$";
+            Pattern compiledPattern2 = Pattern.compile(pattern2);
+            Matcher matcher3 = compiledPattern2.matcher(email);
+            if(matcher3.matches()){
+                System.out.println("Valid email address.");
+            } else {
+                System.out.println("Invalid email address. Please follow the format: abc.xyz@bl.co.in (optional parts: xyz & in)");
+            }
     }
 }
